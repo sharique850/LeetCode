cb87c89e48184764c7532985aa7ea0a8ac39bc3b
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2022-11-11 23:59:06 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (67.98%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        
+        stack<int> st;
+        int mxl=0;
+        
+        st.push(-1);
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+            {
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(st.empty())
+                {
+                    st.push(i);
+                }
+                else{
+                    mxl=max(mxl,i-st.top());
+                }
+            }
+        }
+        return mxl;
+        
+        
+        
+    }
+};
