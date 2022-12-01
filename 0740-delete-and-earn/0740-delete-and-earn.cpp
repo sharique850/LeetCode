595ed08cff1a712981bb37fe048922c985fcd7ee
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2022-12-01 23:38:00 +0530

    Time: 21 ms (73.12%), Space: 14 MB (42.59%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -3,18 +3,31 @@ public:
     int deleteAndEarn(vector<int>& nums) {
         
         int n = 10010;
-        vector<int> freq(n, 0);
-
+        vector<int> value(n,0);
+        
         for(int num:nums)
-            freq[num] += num;
-
-        int prev2=0, prev1=freq[0];
-        for(int i=0; i<n;i++)
+            value[num] += num;
+        int dp[n];
+        dp[1]=value[1];
+        for(int i=2;i<n;i++)
         {
-            int ans = max(freq[i]+prev2, prev1);
-            prev2 = prev1;
-            prev1 = ans;
+            dp[i] = max(dp[i-2] + value[i], dp[i-1]);
         }
-        return prev1;
+        return dp[n-1];
+        
+//         int n = 10010;
+//         vector<int> freq(n, 0);
+
+//         for(int num:nums)
+//             freq[num] += num;
+
+//         int prev2=0, prev1=freq[0];
+//         for(int i=0; i<n;i++)
+//         {
+//             int ans = max(freq[i]+prev2, prev1);
+//             prev2 = prev1;
+//             prev1 = ans;
+//         }
+//         return prev1;
     }
 };
