fdb6c7c5772432b5a0d450ba7bd48e0645937cb4
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2022-12-07 19:55:14 +0530

    Time: 29 ms (81.52%), Space: 11.2 MB (47.92%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    
+    void solveRec(Node *root, vector<int> &ans)
+    {
+        ans.push_back(root->val);
+        
+        for(int i=0; i<root->children.size(); i++)
+        {
+            solveRec(root->children[i], ans);
+        }
+    }
+    vector<int> preorder(Node* root) {
+        
+        vector<int> ans;
+        if(!root) return ans;
+        
+        solveRec(root, ans);
+        
+        return ans;
+        
+    }
+};
