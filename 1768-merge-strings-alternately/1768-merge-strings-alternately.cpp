b1bec517ab2d586d2b0030476c3742dbf4bc6958
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2022-12-14 01:52:49 +0530

    Time: 4 ms (46.49%), Space: 6.3 MB (53.76%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -3,14 +3,15 @@ public:
     string mergeAlternately(string word1, string word2) {
         
         string ans;
-        int i=0,j=0;
+        int i=0;
         int n = word1.size(), m = word2.size();
-        while(i<n || j<m)
+        while(i<n || i<m)
         {
             if(i<n)
-                ans+=word1[i++];
-            if(j<m)
-                ans+=word2[j++];
+                ans+=word1[i];
+            if(i<m)
+                ans+=word2[i];
+            i++;
         }
         
         return ans;
