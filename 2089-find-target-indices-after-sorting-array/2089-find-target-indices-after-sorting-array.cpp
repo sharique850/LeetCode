f16aa445dc5ebcbb31555a36039011c937191c00
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2022-12-23 00:22:20 +0530

    Time: 3 ms (96.50%), Space: 11.7 MB (18.57%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -4,11 +4,25 @@ public:
         
         sort(nums.begin(),nums.end());
         vector<int> ans;
-        for(int i=0;i<nums.size();i++)
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]==target)
+        //         ans.push_back(i);
+        // }
+        // return ans;
+        
+        int small=0, large=0, n=nums.size();
+        for(auto i:nums)
         {
-            if(nums[i]==target)
-                ans.push_back(i);
+            if(i<target)
+                small++;
+            else if(i>target)
+                large++;
         }
+        
+        for(int i=small; i<= n - large - 1; i++)
+            ans.push_back(i);
+        
         return ans;
         
     }
