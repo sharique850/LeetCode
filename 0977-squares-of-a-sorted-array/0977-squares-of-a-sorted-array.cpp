0210ba5da2db51b3550bc90713f8441ca2f455b3
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2022-12-22 21:03:29 +0530

    Time: 39 ms (85.32%), Space: 26.9 MB (16.25%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        
+        vector<int> ans;
+        
+        for(auto i:nums)
+        {
+            ans.push_back(i*i);
+        }
+        sort(ans.begin(), ans.end());
+        
+        return ans;
+        
+    }
+};
