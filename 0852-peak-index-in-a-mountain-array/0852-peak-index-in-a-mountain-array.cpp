0515790d08482b70c386797fed452e664e60e742
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2023-01-02 15:53:05 +0530

    Time: 137 ms (63.82%), Space: 59.8 MB (21.33%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int i =0, j=arr.size()-1, mid;
+        while(i<j){
+            mid=((i+j)/2);
+            if(arr[mid]<arr[mid+1]) i=mid+1;
+            else j=mid;            
+        }
+        return i;
+    }
+};
