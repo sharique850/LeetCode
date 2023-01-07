a2e3ab6629ef4dcda10fe81d63147e9560a85407
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2023-01-07 19:10:46 +0530

    Time: 47 ms (75.11%), Space: 90.2 MB (9.41%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,28 +2,33 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         vector<int> tmp;
-        int idx1=0,idx2=0, n=nums1.size(), m=nums2.size();
-        while(idx1<n && idx2<m)
-        {
-            if(nums1[idx1]<=nums2[idx2])
-                tmp.push_back(nums1[idx1++]);
-            else
-                tmp.push_back(nums2[idx2++]);
-        }
-        while(idx1<n)
-        {
-            tmp.push_back(nums1[idx1++]);
-        }
+//         int idx1=0,idx2=0, n=nums1.size(), m=nums2.size();
+//         while(idx1<n && idx2<m)
+//         {
+//             if(nums1[idx1]<=nums2[idx2])
+//                 tmp.push_back(nums1[idx1++]);
+//             else
+//                 tmp.push_back(nums2[idx2++]);
+//         }
+//         while(idx1<n)
+//         {
+//             tmp.push_back(nums1[idx1++]);
+//         }
         
-        while(idx2<m)
-        {
-            tmp.push_back(nums2[idx2++]);
-        }
-        // for(int ni:nums1)
-        //     tmp.push_
+//         while(idx2<m)
+//         {
+//             tmp.push_back(nums2[idx2++]);
+//         }
         
-        // for(auto p:tmp)
-        //     cout<<p<<" ";
+        
+        for(int n1:nums1)
+            tmp.push_back(n1);
+        
+        for(int n2:nums2)
+            tmp.push_back(n2);
+        
+        sort(tmp.begin(),tmp.end());
+        
         double ans;
         int t=tmp.size();
         if(t%2==0){
