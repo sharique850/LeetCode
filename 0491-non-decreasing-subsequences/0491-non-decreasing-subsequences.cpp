0bd13da903b9fe09ed097355fedd5debc8327b47
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2023-01-20 22:00:38 +0530

    Time: 65 ms (75.53%), Space: 25.5 MB (53.37%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -3,13 +3,10 @@ public:
     vector<vector<int>>ans;
 
     void dfs(vector<int>&nums,vector<int>& temp,int index){
-        // base condition
         if(temp.size()>1) ans.push_back(temp);
         
-        // for each dfs declare unordered_set
         unordered_set<int>set;
         for(int i=index;i<nums.size();i++){
-            // condition
             if((temp.size() == 0 || temp.back()<=nums[i])&&(set.find(nums[i]) == set.end())){
                 temp.push_back(nums[i]);
                 dfs(nums,temp,i+1);
