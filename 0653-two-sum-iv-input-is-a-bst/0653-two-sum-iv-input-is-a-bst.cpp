a57779b55469fa587ac84a8ba6bac24c841e3af9
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2023-01-24 00:39:56 +0530

    Time: 56 ms (45.65%), Space: 38.8 MB (33.33%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -12,28 +12,34 @@
 class Solution {
 public:
     
-    vector<int> v;
+    set<int> s;
+    // vector<int> v;
     
-    void inorder(TreeNode *root)
-    {
-        if(!root) return;
+//     void inorder(TreeNode *root)
+//     {
+//         if(!root) return;
         
-        inorder(root->left);
-        v.push_back(root->val);
-        inorder(root->right);
+//         inorder(root->left);
+//         v.push_back(root->val);
+//         inorder(root->right);
         
-    }
+//     }
     bool findTarget(TreeNode* root, int k) {
         
-        inorder(root);
-        int l=0,r=v.size()-1;
-        while(l<r)
-        {
-            if(v[l]+v[r]==k) return true;
-            else if(v[l]+v[r]<k) l++;
-            else r--;
-        }
-        return false;
+        // inorder(root);
+        // int l=0,r=v.size()-1;
+        // while(l<r)
+        // {
+        //     if(v[l]+v[r]==k) return true;
+        //     else if(v[l]+v[r]<k) l++;
+        //     else r--;
+        // }
+        // return false;
+        
+        if(!root) return false;
+        if(s.find(k-root->val)!=s.end()) return true;
+        s.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right,k);
         
     }
 };
