651706fb7d51f9e7c261516587282607cc0a5722
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2023-01-25 23:55:15 +0530

    Time: 155 ms (98.61%), Space: 97.7 MB (72.85%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -8,16 +8,12 @@ public:
             dfs(neighbor, edges, distance, visited);
         }
     }
-    //we are using DFS to solve this problem
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n = edges.size();
         int ans = -1;
         int minDist = INT_MAX;
-        //dist1 store the distance from node1 to all the neighbour nodes and dist2 store the distance from node2 to all the neighbour nodes
         vector<int> dist1(n, 0), dist2(n, 0);
-        //visited array helps us to identify wheather we visited to particular node form node1 or not similarly for visited2
         vector<bool> visited1(n, false), visited2(n, false);
-        //calling dfs from both the nodes i.e. node1 and node2
         dfs(node1, edges, dist1, visited1);
         dfs(node2, edges, dist2, visited2);
 
